f9a5e8f8c3dcae6c57d32ad17d6dbc3d94a0edb9
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2021-12-01 11:13:03 +0300

    Flatlist render method has been created as a function on the Producs screen || import productCard Fixed ||fixed abbreviation names

--- src/pages/Product/Products/Products.js
+++ src/pages/Product/Products/Products.js
@@ -1,23 +1,24 @@
 import {useNavigation} from '@react-navigation/core';
 import React from 'react';
 import {SafeAreaView, FlatList} from 'react-native';
-import ProductCard from '../../../components/cards/ProductCard/ProductCard';
+import ProductCard from '../../../components/cards/ProductCard/';
 import mockData from '../../../MOCK_DATA.json';
 
 export default function Products() {
   const navigation = useNavigation();
 
   function navigate(selectedProduct) {
-    navigation.navigate('PrdctDtlPage', {product: selectedProduct});
+    navigation.navigate('ProductDetailPage', {product: selectedProduct});
+  }
+  const renderProductCard = ({item}) =>{
+    return(<ProductCard item={item} onSelect={() => navigate(item)} />);
   }
 
   return (
     <SafeAreaView>
       <FlatList
         data={mockData.products}
-        renderItem={({item}) => (
-          <ProductCard item={item} onSelect={() => navigate(item)} />
-        )}
+        renderItem={renderProductCard}
       />
     </SafeAreaView>
   );
